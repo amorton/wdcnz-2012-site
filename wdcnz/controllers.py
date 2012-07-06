d40bc840d96fe1d742d9805e3ae978efa4a542bc
Aaron Morton <aaron@the-mortons.org>
2012-07-07 11:40:05 +1200

    changes

--- wdcnz/controllers.py
+++ wdcnz/controllers.py
@@ -5,7 +5,7 @@ import tornado.web
 class ControllerBase(tornado.web.RequestHandler):
     pass
 
-class Home(ControllerBase):
+class Splash(ControllerBase):
     
     def get(self):
         self.write("howdy")
